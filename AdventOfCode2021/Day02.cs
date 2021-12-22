f6f7dd11692d93202df3065ee00cdac23b90e92a
Keven Denen <KDenen@ahsid.net>
2021-12-22 13:49:08 -0700

    Adding Day 02

---
+++ AdventOfCode2021/Day02.cs
@@ -0,0 +1,63 @@
+﻿internal class Day02
+{
+  // Store directions from input
+  private readonly IEnumerable<DirectionCommand> commands;
+
+  public Day02(string[] inputDirections)
+  {
+    commands = inputDirections.Select(s => s.Split(' ')) 
+                                .Select(s => new DirectionCommand(s[0], int.Parse(s[1])));
+  }
+
+  public int SolvePartOne()
+  {
+    int horizontal = 0;
+    int vertical = 0;
+    foreach (var command in commands)
+    {
+      switch (command.Direction)
+      {
+        case "forward":
+          horizontal += command.Units;
+          break;
+        case "up":
+          vertical -= command.Units;
+          break;
+        case "down":
+          vertical += command.Units;
+          break;
+        default:
+          break;
+      }
+    }
+    return horizontal * vertical;
+  }
+
+  public int SolvePartTwo()
+  {
+    var horizontal = 0;
+    var vertical = 0;
+    var aim = 0;
+    foreach (var direction in commands)
+    {
+      switch (direction.Direction)
+      {
+        case "forward":
+          horizontal += direction.Units;
+          vertical += aim * direction.Units;
+          break;
+        case "up":
+          aim -= direction.Units;
+          break;
+        case "down":
+          aim += direction.Units;
+          break;
+        default:
+          break;
+      }
+    }
+    return vertical * horizontal;
+  }
+
+  private record DirectionCommand(string Direction, int Units);
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -4,3 +4,7 @@ string[] dayOneInput = File.ReadAllLines("./Day01Input.txt");
 var dayOnePuzzle = new Day01(dayOneInput);
 Console.WriteLine($"Day 01 Part 01 Solution - {dayOnePuzzle.SolvePartOne()}");
 Console.WriteLine($"Day 01 Part 02 Solution - {dayOnePuzzle.SolvePartTwo()}");
+string[] dayTwoInput = File.ReadAllLines("./Day02Input.txt");
+var dayTwoPuzzle = new Day02(dayTwoInput);
+Console.WriteLine($"Day 02 Part 01 Solution - {dayTwoPuzzle.SolvePartOne()}");
+Console.WriteLine($"Day 02 Part 02 Solution - {dayTwoPuzzle.SolvePartTwo()}");
