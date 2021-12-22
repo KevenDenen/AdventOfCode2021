213b51892041cf39f7ebaa15f6f8ec586f1ab9fc
Keven Denen <KDenen@ahsid.net>
2021-12-22 10:41:35 -0700

    Add project files.

---
+++ AdventOfCode2021/Day01.cs
@@ -0,0 +1,33 @@
+﻿internal class Day01
+{
+  // Store converted depths data from the input fule
+  private readonly IEnumerable<int> depths;
+
+  // Constructor
+  public Day01(string[] inputDepths)
+  {
+    depths = inputDepths.Select(i => int.Parse(i));
+  }
+
+  public int SolvePartOne()
+  {
+    return CountIncreases(depths);
+  }
+
+  public int SolvePartTwo()
+  {
+    var depthsSliding = depths.Zip(depths.Skip(1), depths.Skip(2)) // Zips the data into a list of tuples of (depths[i], depths[i+1], depths[i+2])
+                              .Select(t => t.First + t.Second + t.Third); // Sums the tuples
+    return CountIncreases(depthsSliding);
+  }
+
+  
+  private static int CountIncreases(IEnumerable<int> depths)
+  {
+    int numberOfIncreases = 0;
+    numberOfIncreases = depths.Zip(depths.Skip(1)) // Zips the data into a list of tuples of (depths[i], depths[i+1])
+                              .Where(t => t.First < t.Second)
+                              .Count();
+    return numberOfIncreases;
+  }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] dayOneInput = File.ReadAllLines("./Day01Input.txt");
+var dayOnePuzzle = new Day01(dayOneInput);
+Console.WriteLine($"Day 01 Part 01 Solution - {dayOnePuzzle.SolvePartOne()}");
+Console.WriteLine($"Day 01 Part 02 Solution - {dayOnePuzzle.SolvePartTwo()}");
